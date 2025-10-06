38ad9fa5a3e3f2dd41a1233393328799569873d5
laura sofia hernandez trillos <sofiatrillos04@gmail.com>
2025-10-06 13:35:00 -0500

    moved files to a new folder

---
+++ Python/base_converter.py
@@ -0,0 +1,25 @@
+from stack import *
+
+def base_converter(dec_num, base):
+    """  
+        This function coverts a decimal number to 
+        any base.
+
+        returns an string 
+    """
+    digits = "0123456789ABCDEF"
+    rem_stack = Stack()
+
+    while dec_num > 0:
+        rem = dec_num % base
+        print(type(rem))
+        rem_stack.push(rem)
+        dec_num = dec_num // base
+    
+    new_string = ""
+    while not rem_stack.is_empty():
+        new_string = new_string + digits[rem_stack.pop()]
+
+    return new_string
+
+print(base_converter(25,16))
